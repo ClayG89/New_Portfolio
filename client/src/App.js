6be3dbc6dccffbd46876a52ead0678a1852702ad
ClayGuess <guessclay@Yahoo.com>
2020-04-20 12:37:46 -0400

    installed libraries in client folder, added react router and added client side routes to app.js. Created components directory in client/src. created components inside of components folder.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import BlogList from "./components/BlogList";
+import Blog from "./components/Blog";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+          <Router>
+              <div className="App">
+
+                  <div>
+                      <h1>Portfolio</h1>
+                      <div>
+                          <div><Link to="/">All Blogs</Link></div>
+                      </div>
+                  </div>
+
+                  <Switch>
+                    <Route exact path="/" component={BlogList}/>
+                    <Route path="/artist/:id" component={Blog}/>
+                  </Switch>
+              </div>
+          </Router>
+      );
+  }
 }
 
 export default App;
+
