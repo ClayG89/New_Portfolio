a35be0f218fb4b848afba59fa40052373fe8932b
ClayGuess <guessclay@Yahoo.com>
2020-04-20 09:52:08 -0400

    Installed django-phone-field. Added phone_field to installed apps in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
-    'portfolio_app'
+    'portfolio_app',
+    'phone_field'
 ]
 
 MIDDLEWARE = [
--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    blog = models.CharField(max_length=1500)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.blogs
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    company = models.CharField(max_length=50)
+    phone = PhoneField(blank=True)
+    email = models.EmailField()
+    topic = models.CharField(max_length=250)
+    message = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
